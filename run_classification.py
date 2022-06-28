14bb19874ae493565d6523083fa5e9acd7d7f021
GSichelero <gabriel.sichelero@hotmail.com>
2022-06-27 21:03:48 -0300

    first commit

---
+++ run_classification.py
@@ -0,0 +1,43 @@
+import numpy as np
+import tensorflow as tf
+import pathlib
+
+
+data_dir = pathlib.Path("./animals")
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
+
+class_names = train_ds.class_names
+
+model = tf.keras.models.load_model('saved_model/animals_cnn')
+model.summary()
+
+
+image_path = input("Enter the name of the image: ")
+image_path = tf.keras.utils.get_file(image_path, origin=f"test_new_images/{image_path}")
+
+img = tf.keras.utils.load_img(
+    image_path, target_size=(img_height, img_width)
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
